6cc81f498e7b5b2f39c51f908332e0dd0d49462a
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-02 12:47:11 -0400

    added utils file for organization

---
+++ code/utils.py
@@ -0,0 +1,9 @@
+import os
+
+
+def open_write_file(dir_path, file_name):
+    """Opens a file for writing, or creates new file if file doesn't exist."""
+    file_path = os.path.join(dir_path, file_name)
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    return file_path
