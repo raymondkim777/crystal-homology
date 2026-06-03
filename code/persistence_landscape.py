77d41cae172a4ecc23c3cb5276e96d5110d7492a
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-03 16:51:22 -0400

    minor changes

--- code/persistence_landscape.py
+++ code/persistence_landscape.py
@@ -2,6 +2,7 @@ import pickle
 from gtda.diagrams import PersistenceLandscape
 import numpy as np
 
+
 def generate_landscape(diagrams, n_layers=2, n_bins=100):
     transformer = PersistenceLandscape(n_layers=n_layers, n_bins=n_bins)
     transformed = transformer.fit_transform([diagrams])
