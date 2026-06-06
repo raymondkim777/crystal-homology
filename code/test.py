163d5af307988049de83fec1f9bc81dab7ad24f0
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-06 15:09:37 -0400

    test

--- code/test.py
+++ code/test.py
@@ -27,24 +27,3 @@ plot_persistence_diagram(diagrams[keys_list[index]])
 
 print(tuple(diagrams.values()))
 diagram_array = np.stack(tuple(diagrams.values()), axis=0) 
-
-# print(type(diagram_array))
-# print(diagram_array.shape)
-
-# landscape = generate_landscape_gtda(diagram_array, plot=True, plot_index=index)
-# landscape = generate_landscape_gtda(diagram_array)
-# print(landscape)
-
-# landscape = generate_landscape_gudhi(diagram_array, plot=True, plot_dim=0, plot_index=index)
-# print(landscape)
-
-# image = generate_image(diagram_array)
-# image = generate_image_gtda(diagram_array, plot=True, plot_index=index)
-# print(image)
-
-# image = generate_image_gudhi(diagram_array)
-# image = generate_image_gudhi(diagram_array, plot=True, plot_index=index)
-# print(image)
-
-# landscape = generate_landscape_gudhi(diagrams[keys_list[index]], plot=True)
-# image = generate_image_gudhi(diagrams[keys_list[index]], plot=True, plot_dim=1)
