80c7c14239a2eee40152c25e2ba9fd2d0b1814f6
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-17 13:25:56 -0400

    .

--- code/cgcnn/train.sh
+++ code/cgcnn/train.sh
@@ -1,3 +1,3 @@
-cp -r ./data/graph_data $SLURM_TMPDIR/graph_data
-# python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --atom-fea-len 128 --n-conv 4 --h-fea-len 128 --n-h 1 --vector landscape data/graph_data
-python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --atom-fea-len 64 --n-conv 5 --h-fea-len 128 --n-h 1 --vector landscape $SLURM_TMPDIR/graph_data
+# cp -r ./data/graph_data $SLURM_TMPDIR/graph_data
+python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --atom-fea-len 128 --n-conv 4 --h-fea-len 128 --n-h 1 --vector landscape data/graph_data
+# python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --atom-fea-len 64 --n-conv 5 --h-fea-len 128 --n-h 1 --vector landscape $SLURM_TMPDIR/graph_data
