a5295cbbfd32d9e6310cd31b87c63b2f6786b6db
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-18 23:36:02 +0000

    .

--- code/cgcnn/train.sh
+++ code/cgcnn/train.sh
@@ -1,3 +1,3 @@
 # python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --atom-fea-len 64 --n-conv 4 --h-fea-len 128 --n-h 2 --vector landscape data/graph_data
 
-python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --id 1 --atom-fea-len 64 --n-conv 4 --h-fea-len 128 --n-h 2 --vector perslay --weight power --phi image data/graph_data
+python main.py --seed --delete --workers 4 --task classification --optim Adam --num-classes 7 --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 --epochs 100 --lr 0.001 --id 0 --atom-fea-len 64 --n-conv 4 --h-fea-len 256 --n-h 2 --vector perslay --weight power --phi image data/graph_data
