0fc8c3d434db4f38af59a5b66c1c13cfd59c4e83
Raymond Kim <raymondkim123456789@gmail.com>
2026-06-25 21:05:51 +0000

    plqy data

--- code/scripts/data_preprocess.sh
+++ code/scripts/data_preprocess.sh
@@ -4,7 +4,8 @@ export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # python get_mp_data.py
-python get_mp_subset.py --seed --subset --size 5500 --large --size-large 7000 --absorb --cif
+# python get_mp_subset.py --seed --subset --size 5500 --large --size-large 7000 --absorb --cif
 
-rm -f data/cif_files.zip
-python -m zipfile -c data/cif_files.zip data/cif/
+# rm -f data/cif_files.zip
+# python -m zipfile -c data/cif_files.zip data/cif/
+
